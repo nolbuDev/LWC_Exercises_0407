6dbe3e77eea510d417feeac6c8cab5bdeef62b5e
nolbu <nolbu.dev@gmail.com>
2020-04-11 17:20:49 +0900

    Exercise 2-4 Completed

---
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -0,0 +1,14 @@
+import { LightningElement, api } from "lwc";
+
+export default class StudentTile extends LightningElement {
+	@api student = {
+		Name: "Nolbu",
+		PhotoUrl: "/services/images/photo/003B0FakePictId"
+	};
+
+	@api selected = false;
+
+	get tileSelected() {
+		return this.selected ? "tile selected " : "tile";
+	}
+}
