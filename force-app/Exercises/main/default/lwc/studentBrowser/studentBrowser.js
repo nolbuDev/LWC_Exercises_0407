90ee498f0ac8fc9ea1d7269c59790d081f9de9e8
nolbu <nolbu.dev@gmail.com>
2020-04-17 16:42:51 +0900

    Exercise 2-7 Completed

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,18 +1,11 @@
-import { LightningElement, track } from "lwc";
+import { LightningElement, track, wire } from "lwc";
+import getStudents from "@salesforce/apex/StudentBrowser.getStudents";
 
 export default class StudentBrowser extends LightningElement {
-	@track studentList = [];
+	@wire(getStudents, { instructorId: "", courseDeliveryId: "" })
+	students;
 
 	constructor() {
 		super();
-		const studentNames = ["Rad", "Stuart", "Andres", "Rahul", "Amit", "Simon"];
-		studentNames.forEach((studentName, index) => {
-			this.studentList.push({
-				sobjectType: "Contact",
-				Name: studentName,
-				PhotoUrl: "/services/images/photo/003B0FakePictId",
-				Id: index
-			});
-		});
 	}
 }
