55b2b1ac119109c89133b199eddd31df826e996c
nolbu <nolbu.dev@gmail.com>
2020-05-10 11:19:56 +0900

    Exercise 4-3 Completed

--- force-app/Exercises/main/default/lwc/certifiedStudentList/certifiedStudentList.js
+++ force-app/Exercises/main/default/lwc/certifiedStudentList/certifiedStudentList.js
@@ -1,5 +1,7 @@
 import { LightningElement, api, track, wire } from "lwc";
 import getCertifiedStudents from "@salesforce/apex/CertifiedStudentList.getCertifiedStudents";
+import deleteStudentCertification from '@salesforce/apex/CertifiedStudentList.deleteStudentCertification';
+import { refreshApex } from '@salesforce/apex';
 
 export default class CertifiedStudentList extends LightningElement {
 	@api certificationId = 0;
@@ -7,9 +9,11 @@ export default class CertifiedStudentList extends LightningElement {
 	@track certifiedStudents;
 	btnGroupDisabled = "disabled";
 	error;
+	_wiredStudentResult;
 
 	@wire(getCertifiedStudents, { certificationId: "$certificationId" })
 	wired_getCertifiedStudents(result) {
+		this._wiredStudentResult = result;
 		this.certifiedStudents = [];
 		if (result.data) {
 			result.data.forEach((student) => {
@@ -54,4 +58,45 @@ export default class CertifiedStudentList extends LightningElement {
 		let numSelected = event.detail.selectedRows.length;
 		this.btnGroupDisabled = numSelected === 0;
 	}
+
+	getSelectedIDs() {
+		let datatable = this.template.querySelector('lightning-datatable');
+		let selectedRows = datatable.getSelectedRows();
+		let ids = [];
+
+		selectedRows.forEach(r => {
+			ids.push(r.certificationHeldId)
+		});
+
+		return ids;
+	}
+	
+	onCertActions (event) {
+		const btnClicked = event.target.getAttribute('data-btn-id');
+		switch (btnClicked) {
+			case 'btnEmail':
+				break;
+			case 'btnSendCert':
+				break;
+			case 'btnDelete':
+				this.onDelete();
+				break;
+			default:
+				break;
+		}
+	}
+	
+	onDelete() {
+		let certificationIds = this.getSelectedIDs();
+		deleteStudentCertification({certificationIds})
+		.then(  () => {
+			refreshApex(this._wiredStudentResult);
+		})
+		.catch(error => {
+			this.error = error;
+		});
+		
+	}
+	
+	
 }
